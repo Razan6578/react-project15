176e320cf0d7b0c84e746bacd1be78822a1b5299
Oleksandr Radionov <fokk190e@gmail.com>
2026-05-11 16:43:41 +0300

    added Register page

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import CategoryCard from './features/mainPage/components/CategoryCard'
 import BlogCard from './features/mainPage/components/BlogCard'
 import PromoBlock from './features/mainPage/components/PromoBlock'
 import BasePageLayout from './shared/layouts/BasePageLayout'
-
-
-/*
-<CategoryCard id={0} name={'FFFFF'} image={'https://placehold.net/400x400.png'} count={2} />
-        <BlogCard id={0} title={'FFFFF'} image={'https://placehold.net/400x400.png'} text={'FFFFF'} />
-        <PromoBlock />
-*/
 
 function App() {
   
--- react-project/src/pages/Registration/index.tsx
+++ react-project/src/pages/Registration/index.tsx
@@ -1,10 +1,77 @@
 import PageLayout from "../../shared/layouts/PageLayout";
 
 const Registration = () =>{
+   const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    
+    // Валідація полів
+
+    const email = formData.get('email') as string;
+    const phone = formData.get('phone') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
+
+    
+    // Перевірка валідності email
+ 
+    if (!emailRegex.test(email)) {
+      alert('Invalid email format');
+      return;
+    }
+
+    // Перевірка валідності телефону
+    const phoneRegex = /^\+?[1-9]\d{1,13}$/;
+    if (!phoneRegex.test(phone)) {
+      alert('Invalid phone number format');
+      return;
+    }
+
+    // Перевірка валідності пароля
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      alert('Password does not meet requirements');
+      return;
+    }
+
+    // Перевірка, чи паролі співпадають
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    localStorage.setItem('user', JSON.stringify({ email, phone, password }));
+  };
+
+
   return (
     <PageLayout name={'Registration'} href={'/registration'}>
-      <h1>Registration</h1>
-      <p>This is the Registration page.</p>
+      <div style={{ margin: '150px', textAlign: 'center' }}>
+        <h1>Registration</h1>
+      
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '20px', maxWidth: '400px' }}>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" pattern={emailRegex.toString()}  required />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone Number:</label>
+            <input type="tel" name="phone" pattern="^\+?[1-9]\d{1,13}$" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" name="confirmPassword" required />
+          </div>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </PageLayout>
   )
 };
